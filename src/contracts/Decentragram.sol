c5dcad6f4777057f8c81cab9b996fa12bd983981
Luke Ohba <lukeohba@gmail.com>
2021-06-12 21:18:56 -0700

    decentagram

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,36 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+  string public name = "Decentragram";
+
+  // Store Images
+   uint public imageCount = 0;
+   mapping(uint => Image) public images;
+
+   struct Image {
+     uint id;
+     string hash;
+     string description;
+     uint tipAmount;
+     address payable author;
+   }
+
+   event ImageCreated(
+     uint id,
+     string hash,
+     string description,
+     uint tipAmount,
+     address payable author
+   );
+
+  // Create Images
+  function uploadImage(string memory _ImgHash, string memory _description) public {
+    require(bytes(_ImgHash).length > 0);
+    require(bytes(_description).length > 0);
+    require(msg.sender != address(0x0));
+    imageCount++;
+    images[imageCount] = Image(imageCount, _ImgHash, _description, 0, msg.sender);
+    emit ImageCreated(imageCount, _ImgHash, _description, 0, msg.sender);
+  }
+  // Tip Images
 }
